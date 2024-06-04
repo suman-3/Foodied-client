5fc12fbcb94133a371a021365fb23ce47a8be54d
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-04 23:21:21 +0530

    list product based on filter done by params

--- app/(routes)/menu/_components/page-content.tsx
+++ app/(routes)/menu/_components/page-content.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { PopularContent } from "@/components/root/popular-content";
 import { Box } from "@/components/shared/box";
 import { Product } from "@/types-db";
-import { Home } from "lucide-react";
+import { ChevronRight, Home, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import qs from "query-string";
@@ -30,13 +31,66 @@ export const PageContent = ({ products }: PageContentProps) => {
   };
 
   return (
-    <Box className="pt-4 pb-24 flex-col items-start">
-      <Box className="text-neutral-700 text-sm items-center">
-        <Link href={"/"} className="flex items-center gap-2">
-          <Home className="w-4 h-4 " />
-          Main Page
-        </Link>
+    <>
+      <Box className="pt-4 pb-24 flex-col items-start">
+        <Box className="text-neutral-700 text-sm items-center">
+          <Link href={"/"} className="flex items-center gap-2">
+            <Home className="w-4 h-4 " />
+            Main Page
+          </Link>
+          <ChevronRight className="w-5 h-5 text-muted-foreground" />
+          <Link href={"/menu"} className="flex items-center gap-2">
+            Products
+          </Link>
+
+          {searchParams.get("category") && (
+            <>
+              <ChevronRight className="w-5 h-5 text-muted-foreground" />
+              <Link href={"/menu"} className="flex items-center gap-2">
+                {searchParams.get("category")}
+              </Link>
+            </>
+          )}
+        </Box>
+
+        <Box className="mt-8 flex flex-col items-start ">
+          {searchParams.get("category") && (
+            <h2 className="flex items-center gap-2 text-3xl font-semibold text-neutral-700">
+              {searchParams.get("category")}
+            </h2>
+          )}
+        </Box>
+
+        <Box className="gap-3 my-4">
+          {currentParams &&
+            Object.entries(currentParams).map(([key, value]) => (
+              <div
+                key={key}
+                className="px-4 py-1 cursor-pointer hover:shadow-md rounded-md bg-emerald-500/10 text-neutral-600 flex items-center gap-1"
+                onClick={() => handleClick(key)}
+              >
+                {value}
+                <X className="w-4 h-4" />
+              </div>
+            ))}
+        </Box>
       </Box>
-    </Box>
+
+      <div className="grid grid-cols-2 lg:grid-cols-3 w-full h-full gap-4 gap-y-24">
+        {products.length > 0 ? (
+          <>
+            {products.map((product) => (
+              <PopularContent data={product} key={product.id} />
+            ))}
+          </>
+        ) : (
+          <>
+            <Box className="items-center justify-center py-12 text-muted-foreground font-semibold text-2xl col-span-10">
+              No Products Avaliable
+            </Box>
+          </>
+        )}
+      </div>
+    </>
   );
 };
